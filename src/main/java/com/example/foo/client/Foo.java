fa1e66dae18ef1e8ce6db9b45a554e8b0648081b
Alan TORO <alan.toro@relatech.com>
2025-03-28 10:13:05 -0400

    use button from the same lib

--- src/main/java/com/example/foo/client/Foo.java
+++ src/main/java/com/example/foo/client/Foo.java
@@ -11,13 +11,7 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.*;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -163,5 +157,9 @@ public class Foo implements EntryPoint {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<sl-button slot=\"footer\" variant=\"primary\">Close</sl-button>");
     dialog.setPropertyBoolean("open", true);
     RootPanel.get().getElement().appendChild(dialog);
+
+    final String htmlContent = "<sl-button variant='primary' onclick='alert(\"Bottoncino premuto!\")'>Clicca me</sl-button>";
+    HTMLPanel panel = new HTMLPanel(htmlContent);
+    RootPanel.get("container").add(panel);
   }
 }
